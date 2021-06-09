070e87c194ac378f06eec9829bbf6a1a2e071f37
Nikita Kasyanov <kasyanikita@gmail.com>
2021-06-09 15:26:30 +0300

    commit 1

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -10,43 +10,43 @@ class TimedDoor;
 
 class TimerClient {
  public:
-  virtual void Timeout() = 0;
+    virtual void Timeout() = 0;
 };
 
 class Door {
  public:
-  virtual void lock() = 0;
-  virtual void unlock() = 0;
-  virtual bool isDoorOpened() = 0;
+    virtual void lock() = 0;
+    virtual void unlock() = 0;
+    virtual bool isDoorOpened() = 0;
 };
 
 class DoorTimerAdapter : public TimerClient {
  private:
-  TimedDoor& door;
+    TimedDoor& door;
  public:
-  explicit DoorTimerAdapter(TimedDoor&);
-  void Timeout();
+    explicit DoorTimerAdapter(TimedDoor&);
+    void Timeout();
 };
 
 class TimedDoor : public Door {
  private:
-  DoorTimerAdapter * adapter;
-  int iTimeout;
-  bool opened;
+    DoorTimerAdapter * adapter;
+    int iTimeout;
+    bool opened;
  public:
-  explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
-  void DoorTimeOut();
-  void throwState();
+    explicit TimedDoor(int);
+    bool isDoorOpened();
+    void unlock();
+    void lock();
+    void DoorTimeOut();
+    void throwState();
 };
 
 class Timer {
-  TimerClient *client;
-  void sleep(int);
+    TimerClient *client;
+    void sleep(int);
  public:
-  void tregister(int, TimerClient*);
+    void tregister(int, TimerClient*);
 };
 
 #endif  // INCLUDE_TIMEDDOOR_H_
---
+++ src/TimedDoor.cpp
@@ -0,0 +1,52 @@
+// Copyright 2021 GHA Test Team
+
+#include "TimedDoor.h"
+#include <chrono>
+#include <thread>
+
+TimedDoor::TimedDoor(int time) {
+    iTimeout = time;
+    adapter = new DoorTimerAdapter(*this);
+    opened = false;
+}
+
+void TimedDoor::unlock() {
+    Timer timer;
+    opened = true;
+    timer.tregister(iTimeout, adapter);
+}
+
+void Timer::tregister(int sleepTime, TimerClient *adapter) {
+    sleep(sleepTime);
+    adapter->Timeout();
+}
+
+void Timer::sleep(int sleepTime) {
+    std::this_thread::sleep_for(std::chrono::seconds(sleepTime));
+}
+
+void DoorTimerAdapter::Timeout() {
+    door.DoorTimeOut();
+}
+
+void TimedDoor::DoorTimeOut() {
+    throw std::string("close the door!");
+}
+
+void TimedDoor::throwState() {
+    if (isDoorOpened()) {
+        throw std::string("the door is open!");
+    } else {
+        throw std::string("the door is closed!");
+    }
+}
+
+bool TimedDoor::isDoorOpened() {
+    return opened;
+}
+
+void TimedDoor::lock() {
+    opened = false;
+}
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor & timedDoor): door(timedDoor) {}
